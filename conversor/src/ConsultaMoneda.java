be3391690ea31194fa87c961bc1c411e055545f4
Jocelyn <jocelyn.tirado6677@alumnos.udg.mx>
2025-07-22 21:34:45 -0600

    Refactor Moneda class to use record type and update buscaMoneda method for JSON parsing

--- conversor/src/ConsultaMoneda.java
+++ conversor/src/ConsultaMoneda.java
@@ -4,6 +4,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import com.google.gson.Gson;
 
+
 public class ConsultaMoneda {
     public Moneda buscaMoneda (String moneda){
         URI direccion =  URI.create("https://v6.exchangerate-api.com/v6/a613d999f3f5aa4584c92f21/latest/" + moneda);
@@ -14,7 +15,8 @@ public class ConsultaMoneda {
             .send(request, HttpResponse.BodyHandlers.ofString());
         //String json = response.body();
         //System.out.println(json);
-        return new Gson().fromJson(response.body(), Moneda.class);
+        Map<String, Moneda> monedas = new Gson().fromJson(response.body(), new TypeToken<Map<String, Moneda>>(){}.getType());
+        return monedas.get(moneda);
         }
         catch (Exception e) {
             throw new RuntimeException("No se pudo obtener la moneda: " + moneda, e);
--- conversor/src/Moneda.java
+++ conversor/src/Moneda.java
@@ -1,19 +1,2 @@
-public class Moneda {
-    private ConversionRates conversion_rates;
-
-    public ConversionRates getConversion_rates() {
-        return conversion_rates;
-    }
-
-    public static class ConversionRates {
-        private double ARS;
-        private double COP;
-        private double BRL;
-        private double USD;
-
-        public double getARS() { return ARS; }
-        public double getCOP() { return COP; }
-        public double getBRL() { return BRL; }
-        public double getUSD() { return USD; }
-    }
+public record Moneda (String usd, String ars, String brl, String cop){
 }
