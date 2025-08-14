959cedc67077aaa6f415a72bf3a023f832f18c2b
Jocelyn <jocelyn.tirado6677@alumnos.udg.mx>
2025-08-14 02:43:35 -0600

    validaciones y otros cambios

--- src/App.java
+++ src/App.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("**********************************");
+        System.out.println("Bienvenido al conversor de moneda :)\n");
         Conversor conversor = new Conversor();
-        Scanner scanner = new Scanner(System.in);
+        
         int opcion = 0;
         while (opcion != 7){
             conversor.menu();
-            opcion = scanner.nextInt();
+            opcion = conversor.scanner.nextInt();
             conversor.conversion(opcion);
         }
-        scanner.close();
+        conversor.scanner.close();
     }
     
 }
--- src/Conversor.java
+++ src/Conversor.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 public class Conversor {
     public int opcion;
+    public Scanner scanner = new Scanner(System.in);
 
     public void menu(){
-        System.out.println("**********************************");
-        System.out.println("Bienvenido al conversor de moneda :)\n");
         System.out.println("-------------------CAMBIOS-------------------");
         System.out.printf("1) Dolar             --->    Peso Argentino\n");
         System.out.printf("2) Peso Argentino    --->    Dolar\n");
@@ -18,48 +17,57 @@ public class Conversor {
     }
     public void conversion (int opcion){
         Moneda usd = new ConsultaMoneda().buscaMoneda("USD");
-        Moneda ars = new ConsultaMoneda().buscaMoneda("ARS");
-        Moneda brl = new ConsultaMoneda().buscaMoneda("BRL");
-        Moneda cop = new ConsultaMoneda().buscaMoneda("COP");
         double monedaDeCambio;
-        Scanner scanner = new Scanner(System.in);
         switch (opcion) {
             case 1:
                 System.out.println("Ingrese la cantidad de dolares a convertir: ");
-                monedaDeCambio = scanner.nextDouble();
+                monedaDeCambio = leerCantidad();
                 System.out.printf("%.2f dolares son %.2f pesos argentinos\n\n\n", monedaDeCambio, monedaDeCambio * Double.parseDouble(usd.ars()));
                 break;
             case 2:
+                Moneda ars = new ConsultaMoneda().buscaMoneda("ARS");
                 System.out.println("Ingrese la cantidad de pesos argentinos a convertir: ");
-                monedaDeCambio = scanner.nextDouble();
+                monedaDeCambio = leerCantidad();
                 System.out.printf("%.2f pesos argentinos son %.2f dolares\n\n\n", monedaDeCambio, monedaDeCambio * Double.parseDouble(ars.usd()));
                 break;
             case 3:
                 System.out.println("Ingrese la cantidad de dolares a convertir: ");
-                monedaDeCambio = scanner.nextDouble();
+                monedaDeCambio = leerCantidad();
                 System.out.printf("%.2f dolares son %.2f reales brasileños\n\n\n", monedaDeCambio, monedaDeCambio * Double.parseDouble(usd.brl()));
                 break;
             case 4:
+                Moneda brl = new ConsultaMoneda().buscaMoneda("BRL");
                 System.out.println("Ingrese la cantidad de reales brasileños a convertir: ");
-                monedaDeCambio = scanner.nextDouble();
+                monedaDeCambio = leerCantidad();
                 System.out.printf("%.2f reales brasileños son %.2f dolares\n\n\n", monedaDeCambio, monedaDeCambio * Double.parseDouble(brl.usd()));
                 break;
             case 5:
                 System.out.println("Ingrese la cantidad de dolares a convertir: ");
-                monedaDeCambio = scanner.nextDouble();
+                monedaDeCambio = leerCantidad();
                 System.out.printf("%.2f dolares son %.2f pesos colombianos\n\n\n", monedaDeCambio, monedaDeCambio * Double.parseDouble(usd.cop()));
                 break;
             case 6:
+                Moneda cop = new ConsultaMoneda().buscaMoneda("COP");
                 System.out.println("Ingrese la cantidad de pesos colombianos a convertir: ");
-                monedaDeCambio = scanner.nextDouble();
+                monedaDeCambio = leerCantidad();
                 System.out.printf("%.2f pesos colombianos son %.2f dolares\n\n\n", monedaDeCambio, monedaDeCambio * Double.parseDouble(cop.usd()));
                 break;
             case 7:
                 System.out.println("Saliendo del conversor...");
-                scanner.close();
                 break;
             default:
                 System.out.println("Opcion no valida.");
+        }
+    }
+
+    public double leerCantidad() {
+        while (true) {
+            if (scanner.hasNextDouble()) {
+                return scanner.nextDouble();
+            } else {
+                System.out.print("Debe ingresar un número válido. Intente de nuevo: ");
+                scanner.next();
+            }
         }
     }
 }
